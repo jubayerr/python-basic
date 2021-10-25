f44d53c1d13ce88dab803adbb0914591c8e87384
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-25 11:42:09 +0600

    conditions

---
+++ conditions.py
@@ -0,0 +1,11 @@
+# conditions
+
+is_student = True
+is_regular = True
+
+if is_student and is_regular:
+    print("You are welcome!")
+elif is_student and not is_regular:
+    print("You are not regular.")
+else:
+    print("You have to wait.")
