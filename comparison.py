a2451327a7a0dbfe27520e3c2c900d37a451876d
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-25 11:50:49 +0600

    comparisons

---
+++ comparison.py
@@ -0,0 +1,14 @@
+# comparisons
+
+def max_max(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+result = max(2, 3, 4)
+
+print(result)
