2a0ca9154c9e01a7aa0f4adbb9a9fd45527fcffa
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-25 10:53:09 +0600

    function

---
+++ functions.py
@@ -0,0 +1,10 @@
+# functions
+
+def say_hi(name, age):
+    print("Hello " + name + ". You are " + age + " years old.")
+
+
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+say_hi(user_name, user_age)
