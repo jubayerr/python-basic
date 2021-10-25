2bc9f4fefe68e8036a88e783e8757dda73cc26b8
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-25 10:12:29 +0600

    list function

---
+++ listFunction.py
@@ -0,0 +1,20 @@
+# lists function
+
+friends = ['Jonas', 'Martha', 'Bartosz', 'Mikkel', 'Magnus', 'Franziska']
+
+friends.remove('Mikkel')
+friends.count('Jonas')
+
+friends.sort()
+
+# friends.pop()
+# friends.clear()
+
+friends2 = friends.copy()
+friends2.reverse()
+
+print(friends)
+print(friends2)
+
+
+
