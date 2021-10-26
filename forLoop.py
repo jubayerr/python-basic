8c357aa7c626bbe26ff72e6886bf6e642b07c0d1
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-26 11:23:52 +0600

    for loop

---
+++ forLoop.py
@@ -0,0 +1,19 @@
+# for loop
+
+friends = ["Jonas", "Bartosz", "Mikkel"]
+
+
+for friend in friends:
+    print(friend)
+
+
+for letter in "Monkey":
+    print(letter)
+
+
+for index in range(10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
