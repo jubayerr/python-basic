121e2538fd80ac312d66569ecf3cc5e5ed9b3c3c
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-26 15:38:21 +0600

    2d lists and nested loops

---
+++ 2dLists.py
@@ -0,0 +1,19 @@
+# 2 dimensional lists
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+res = number_grid[0]
+
+print(res)
+
+# nested for loop
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
