b35adaab70a7e7f2778ff28d0c6f2978540fe0bd
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-27 12:34:01 +0600

    classes and objects

---
+++ classesAndObjects.py
@@ -0,0 +1,12 @@
+# classes and objects
+
+from student import Student
+
+student1 = Student("Mike", "Business", 3.2, False)
+student2 = Student("Harry", "Magic", 3.8, False)
+
+print("Student Name: ", student1.name)
+print("Student Major: ", student1.major)
+print("Student GPA: ", student1.gpa)
+
+# ...end
