3faa2a0f028e124628b817639c09e2627db46c85
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-27 10:52:53 +0600

    writing file

---
+++ writingFile.py
@@ -0,0 +1,14 @@
+# writing file
+
+# employee_file = open("employees2.txt", "w")
+#
+# for index in range(10):
+#     employee_file.write("\nhello")
+
+employee_file = open("employees.txt", "a")
+
+employee_file.write("\nIris - Market Analysis")
+
+
+employee_file.close()
+
