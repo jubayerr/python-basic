61b898ed8365ae56b24f831bd5590741fd84a86a
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-27 10:18:03 +0600

    reading file

---
+++ readingFiles.py
@@ -0,0 +1,19 @@
+# reading files
+
+employee_file = open("employees", "r")
+
+# print(employee_file.readable())
+
+# print(employee_file.read())
+
+# print(employee_file.readline())
+
+# print(employee_file.readlines())
+
+# print(employee_file.readlines()[1])
+
+for employee in employee_file.readlines():
+    print(employee)
+
+employee_file.close()
+
